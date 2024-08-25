026effd5374fbfba668d114eeb15119868140f20
Vitaly <vitalikd13@gmail.com>
2024-08-25 22:54:18 +0300

    feat: show connect btn

--- client/src/pages/ConnectWallet/ConnectWallet.js
+++ client/src/pages/ConnectWallet/ConnectWallet.js
@@ -1,42 +1,197 @@
-import React, { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useLocation } from 'react-router-dom';
-import Swal from 'sweetalert2'
-import globalContext from './../../context/global/globalContext'
-import LoadingScreen from '../../components/loading/LoadingScreen'
+import React, { useContext, useEffect, useState, useRef, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import Swal from "sweetalert2";
+import LoadingScreen from "../../components/loading/LoadingScreen";
+import socketContext from "../../context/websocket/socketContext";
+import { CS_FETCH_LOBBY_INFO } from "../../pokergame/actions";
+import { connectMetamask } from "../../utils/interact";
+import globalContext from "./../../context/global/globalContext";
+import "./ConnectWallet.scss";
 
-import socketContext from '../../context/websocket/socketContext'
-import { CS_FETCH_LOBBY_INFO } from '../../pokergame/actions'
-import './ConnectWallet.scss'
+/** @returns {URLSearchParams} */
+const useQuery = () => {
+	const { search } = useLocation();
+	return useMemo(() => new URLSearchParams(search), [search]);
+};
+
+const useJoinGame = () => {
+	const { setWalletAddress, setChipsAmount } = useContext(globalContext);
+	const { socket } = useContext(socketContext);
+	const navigate = useNavigate();
+
+	/**
+	 * @param {string} walletAddress
+	 * @param {string} gameId
+	 * @param {string} username
+	 * @returns {Promise<void>}
+	 */
+	return async (walletAddress, gameId, username) => {
+		try {
+			console.log({ walletAddress, gameId, username });
+			if (!walletAddress || !gameId || !username) {
+				throw new Error("Invalid parameters");
+			}
+
+			setWalletAddress(walletAddress);
+			socket.emit(CS_FETCH_LOBBY_INFO, {
+				walletAddress,
+				socketId: socket.id,
+				gameId,
+				username,
+			});
+			console.log(CS_FETCH_LOBBY_INFO, {
+				walletAddress,
+				socketId: socket.id,
+				gameId,
+				username,
+			});
+
+			navigate("/play");
+		} catch (error) {
+			console.log(error);
+			return error;
+		}
+	};
+};
+
+const ConnectMetamaskButton = ({ callback }) => {
+	const [loading, setLoading] = useState(false);
+
+	return (
+		<button
+			type="button"
+			className="btn btn-primary"
+			onClick={async () => {
+				setLoading(true);
+				try {
+					const { event, response } = await connectMetamask();
+					if (event !== "connected") {
+						throw new Error("Metamask not connected");
+					}
+					callback(response);
+				} catch (error) {
+					console.log(error);
+				} finally {
+					setLoading(false);
+				}
+			}}
+		>
+			{loading ? "Connecting..." : "Connect Metamask"}
+		</button>
+	);
+};
 
 const ConnectWallet = () => {
-  const { setWalletAddress, setChipsAmount } = useContext(globalContext)
-   
-  const { socket } = useContext(socketContext)
-  const navigate = useNavigate()
-  const useQuery = () => new URLSearchParams(useLocation().search);
-  let query = useQuery()
+	const { socket } = useContext(socketContext);
 
-  useEffect(() => {
-    if(socket !== null && socket.connected === true){
-      const walletAddress = query.get('walletAddress')
-      const gameId = query.get('gameId')
-      const username = query.get('username')
-      if(walletAddress && gameId && username){
-        console.log(username)
-        setWalletAddress(walletAddress)
-        socket.emit(CS_FETCH_LOBBY_INFO, { walletAddress, socketId: socket.id, gameId, username })
-        console.log(CS_FETCH_LOBBY_INFO, { walletAddress, socketId: socket.id, gameId, username })
-        navigate('/play')
-      }
-    }
-  }, [socket])
+	const [walletAddress, setWalletAddress] = useState("");
+	const [username, setUsername] = useState("");
+	const [loading, setLoading] = useState(false);
+	const addressRef = useRef();
 
-  return (
-    <>
-      <LoadingScreen />
-    </>
-  )
-}
+	const joinGame = useJoinGame();
 
-export default ConnectWallet
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log("Connecting...");
+		setLoading(true);
+
+		// TODO: get gameId from API - `getPokerTables`
+		const gameId = "123"; // query.get("gameId");
+
+		joinGame(walletAddress, gameId, username)
+			.then(() => {
+				setLoading(false);
+			})
+			.catch((error) => {
+				setLoading(false);
+				Swal.fire({
+					title: "Error",
+					text: error.message,
+					icon: "error",
+					confirmButtonText: "Ok",
+				});
+			});
+	};
+
+	// useEffect(() => {
+	// 	if (socket !== null && socket.connected === true) {
+	// 		console.log("Connecting...");
+	// 		const walletAddress = query.get("walletAddress");
+	// 		const gameId = query.get("gameId");
+	// 		const username = query.get("username");
+	// 		if (walletAddress && gameId && username) {
+	// 			console.log(username);
+	// 			setWalletAddress(walletAddress);
+	// 			socket.emit(CS_FETCH_LOBBY_INFO, {
+	// 				walletAddress,
+	// 				socketId: socket.id,
+	// 				gameId,
+	// 				username,
+	// 			});
+	// 			console.log(CS_FETCH_LOBBY_INFO, {
+	// 				walletAddress,
+	// 				socketId: socket.id,
+	// 				gameId,
+	// 				username,
+	// 			});
+	// 			navigate("/play");
+	// 		}
+	// 	}
+	// }, [socket]);
+
+	if (socket === null || !socket.connected) {
+		return <LoadingScreen />;
+	}
+
+	if (loading) {
+		return <LoadingScreen />;
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className="connect-wallet-area">
+				<div className="connect-wallet-form">
+					<div className="form-group">
+						<label htmlFor="walletAddress">Wallet Address</label>
+						{walletAddress ? (
+							<input
+								type="text"
+								defaultValue={walletAddress}
+								className="form-control"
+								id="walletAddress"
+								name="walletAddress"
+								placeholder="Enter Wallet Address"
+								required
+							/>
+						) : (
+							<div>
+								<ConnectMetamaskButton callback={setWalletAddress} />
+							</div>
+						)}
+					</div>
+					<div className="form-group">
+						<label htmlFor="username">Username</label>
+						<input
+							type="text"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							className="form-control"
+							id="username"
+							name="username"
+							placeholder="Enter Username"
+							required
+						/>
+					</div>
+					<button type="submit" className="btn btn-primary mt-auto">
+						Connect
+					</button>
+				</div>
+			</div>
+		</form>
+	);
+};
+
+export default ConnectWallet;
--- client/src/utils/interact.js
+++ client/src/utils/interact.js
@@ -1,40 +1,44 @@
-import React from 'react'
-import { ethers } from 'ethers'
+import { ethers } from "ethers";
+import React from "react";
 
 export const connectMetamask = async () => {
-  const chainId = process.env.REACT_APP_CHAIN_ID;
+	const chainId = "0x1"; //process.env.REACT_APP_CHAIN_ID;
 
-  if (window.ethereum) {
-    try {
-      const currentChain = await window.ethereum.request({
-        method: 'eth_chainId',
-      })
-      const addressArr = await window.ethereum.request({
-        method: 'eth_requestAccounts',
-      })
+	if (window.ethereum) {
+		try {
+			const currentChain = await window.ethereum.request({
+				method: "eth_chainId",
+			});
 
-      if (parseInt(currentChain, 16) == chainId) {
-        return { event: 'connected', response: addressArr[0] }
-      } else {
-        console.log(currentChain, chainId)
-        console.log('plz switch your network!')
-        await window.ethereum.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{chainId: chainId}]
-        })
-        
-        const addressArr = await window.ethereum.request({
-          method: 'eth_requestAccounts',
-        })
-        
-        return { event: 'connected', response: addressArr[0] }
-        // return { event: 'Wrong Chain', response: currentChain }
-      }
-    } catch (err) {
-      console.log(err.message)
-    }
-  } else {
-    console.log('plz install metamask on your browser')
-    return { event: 'No Wallet', response: 'plz install metamask on your browser'}
-  }
-}
+			if (Number.parseInt(currentChain, 16) === Number.parseInt(chainId, 16)) {
+				const addressArr = await window.ethereum.request({
+					method: "eth_requestAccounts",
+				});
+
+				return { event: "connected", response: addressArr[0] };
+			}
+
+			console.log(currentChain, chainId);
+			console.log("plz switch your network!");
+			await window.ethereum.request({
+				method: "wallet_switchEthereumChain",
+				params: [{ chainId: chainId }],
+			});
+
+			const addressArr = await window.ethereum.request({
+				method: "eth_requestAccounts",
+			});
+
+			return { event: "connected", response: addressArr[0] };
+			// return { event: 'Wrong Chain', response: currentChain }
+		} catch (err) {
+			console.log(err.message);
+		}
+	} else {
+		console.log("plz install metamask on your browser");
+		return {
+			event: "No Wallet",
+			response: "plz install metamask on your browser",
+		};
+	}
+};
